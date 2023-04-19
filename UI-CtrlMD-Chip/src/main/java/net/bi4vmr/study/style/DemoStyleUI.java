ac8e334020eb0ef920e3fe168e03cbe3d8b2a0c2
BI4VMR <bi4vmr@outlook.com>
2023-04-19 14:33:49 +0800

    0CE18F11-C555-48DB-B2C6-365671B61885

--- UI-CtrlMD-Chip/src/main/java/net/bi4vmr/study/style/DemoStyleUI.java
+++ UI-CtrlMD-Chip/src/main/java/net/bi4vmr/study/style/DemoStyleUI.java
@@ -1,11 +1,8 @@
 package net.bi4vmr.study.style;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.chip.Chip;
 
 import net.bi4vmr.study.R;
 
@@ -15,26 +12,5 @@ public class DemoStyleUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_demo_style);
-
-        Chip chipAction = findViewById(R.id.chipAction);
-        Chip chipFilter = findViewById(R.id.chipFilter);
-        Chip chipEntry = findViewById(R.id.chipEntry);
-
-        // 点击事件监听器
-        chipAction.setOnClickListener(v -> {
-            // 打印日志
-            Log.i("myapp", "Chip被点击。");
-        });
-
-        // 选中事件监听器
-        chipFilter.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            // 打印日志
-            Log.i("myapp", "Chip的选中状态发生改变，新状态：" + isChecked);
-        });
-
-        chipEntry.setOnCloseIconClickListener(v -> {
-            // 打印日志
-            Log.i("myapp", "Chip的关闭按钮被点击。");
-        });
     }
 }
