de2c750d441205e76b4729366c318159e750b3e4
BI4VMR <bi4vmr@outlook.com>
2023-06-02 16:55:10 +0800

    8C7EBFA2-481C-4CAF-8529-0A2F26A675A6

--- Net-Common-ConnectState/src/main/java/net/bi4vmr/study/listenstate/DemoListenStateUI.java
+++ Net-Common-ConnectState/src/main/java/net/bi4vmr/study/listenstate/DemoListenStateUI.java
@@ -5,6 +5,7 @@ import android.net.LinkProperties;
 import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.net.NetworkRequest;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -13,6 +14,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import net.bi4vmr.study.R;
+
+import java.util.Arrays;
 
 public class DemoListenStateUI extends AppCompatActivity {
 
@@ -32,10 +35,12 @@ public class DemoListenStateUI extends AppCompatActivity {
 
         // 注册网络状态回调
         btnRegister.setOnClickListener(v -> {
+            // 构造NetworkRequest对象，设置感兴趣的网络。
             NetworkRequest request = new NetworkRequest.Builder()
                     .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
                     .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
                     .build();
+            // 注册回调
             manager.registerNetworkCallback(request, callback);
         });
 
@@ -50,45 +55,49 @@ public class DemoListenStateUI extends AppCompatActivity {
 
         @Override
         public void onAvailable(@NonNull Network network) {
-            super.onAvailable(network);
             Log.i("myapp", "MyNetworkCallback:OnAvailable.");
-
         }
 
         @Override
         public void onLosing(@NonNull Network network, int maxMsToLive) {
-            super.onLosing(network, maxMsToLive);
             Log.i("myapp", "MyNetworkCallback:OnLosing.");
         }
 
         @Override
         public void onLost(@NonNull Network network) {
-            super.onLost(network);
             Log.i("myapp", "MyNetworkCallback:OnLost.");
         }
 
         @Override
         public void onUnavailable() {
-            super.onUnavailable();
             Log.i("myapp", "MyNetworkCallback:OnUnavailable.");
         }
 
         @Override
         public void onCapabilitiesChanged(@NonNull Network network, @NonNull NetworkCapabilities networkCapabilities) {
-            super.onCapabilitiesChanged(network, networkCapabilities);
             Log.i("myapp", "MyNetworkCallback:OnCapabilitiesChanged.");
+            // 获取当前协议协商的上行带宽
+            String BWup = networkCapabilities.getLinkDownstreamBandwidthKbps() + " Kbps";
+            String BWdown = networkCapabilities.getLinkDownstreamBandwidthKbps() + " Kbps";
+            Log.i("myapp", "MyNetworkCallback:OnCapabilitiesChanged. BandwidthUP: " + BWup);
+            Log.i("myapp", "MyNetworkCallback:OnCapabilitiesChanged. BandwidthDown: " + BWdown);
+
+            // 获取当前网络的能力集
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                Log.i("myapp", "MyNetworkCallback:OnCapabilitiesChanged. All: " +
+                        Arrays.toString(networkCapabilities.getCapabilities()));
+            }
         }
 
         @Override
         public void onLinkPropertiesChanged(@NonNull Network network, @NonNull LinkProperties linkProperties) {
-            super.onLinkPropertiesChanged(network, linkProperties);
             Log.i("myapp", "MyNetworkCallback:OnLinkPropertiesChanged.");
+            Log.i("myapp", "MyNetworkCallback:OnLinkPropertiesChanged. Interface:" + linkProperties.getInterfaceName());
         }
 
         @Override
         public void onBlockedStatusChanged(@NonNull Network network, boolean blocked) {
-            super.onBlockedStatusChanged(network, blocked);
-            Log.i("myapp", "MyNetworkCallback:OnBlockedStatusChanged.");
+            Log.i("myapp", "MyNetworkCallback:OnBlockedStatusChanged. IsBlocked:" + blocked);
         }
     }
 }
