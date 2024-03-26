8c88957dd552ebf4355eb575d4291db8dd7c6fb7
BI4VMR <bi4vmr@outlook.com>
2024-03-26 18:00:16 +0800

    D93DCAB6-0B4D-4F48-98FF-9EACBD823529

--- M03_UI/C01_Common/S02_Activity/src/main/java/net/bi4vmr/study/MainActivity.java
+++ M03_UI/C01_Common/S02_Activity/src/main/java/net/bi4vmr/study/MainActivity.java
@@ -3,7 +3,6 @@ package net.bi4vmr.study;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,50 +24,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // 启动新的Activity
-        Button btnSwitchPage = findViewById(R.id.btnSwitchPage);
-        btnSwitchPage.setOnClickListener(v -> {
+        binding.btnSwitchPage.setOnClickListener(v -> {
             Intent intent = new Intent(this, DemoGotoPageUI.class);
             startActivity(intent);
         });
 
         // 启动Activity并等待返回结果（旧方法）
-        Button btnGotoForResult = findViewById(R.id.btnGotoForResult);
-        btnGotoForResult.setOnClickListener(v -> {
+        binding.btnGotoForResult.setOnClickListener(v -> {
             Intent intent = new Intent(this, DemoGotoForResultUI.class);
             startActivity(intent);
         });
 
         // 启动Activity并等待返回结果（新方法）
-        Button btnGotoForResult2 = findViewById(R.id.btnGotoForResult2);
-        btnGotoForResult2.setOnClickListener(v -> {
+        binding.btnGotoForResult2.setOnClickListener(v -> {
             Intent intent = new Intent(this, DemoGotoForResult2UI.class);
             startActivity(intent);
         });
 
         // 生命周期
-        Button btnLifeCycle = findViewById(R.id.btnLifeCycle);
-        btnLifeCycle.setOnClickListener(v -> {
+        binding.btnLifeCycle.setOnClickListener(v -> {
             Intent intent = new Intent(this, DemoLifeCycleUI.class);
             startActivity(intent);
         });
 
         // 视图数据保持
-        Button btnViewState = findViewById(R.id.btnViewState);
-        btnViewState.setOnClickListener(v -> {
+        binding.btnViewState.setOnClickListener(v -> {
             Intent intent = new Intent(this, DemoViewStateUI.class);
             startActivity(intent);
         });
 
         // 处理配置变更
-        Button btnConfig = findViewById(R.id.btnConfig);
-        btnConfig.setOnClickListener(v -> {
+        binding.btnConfig.setOnClickListener(v -> {
             Intent intent = new Intent(this, DemoConfigUI.class);
             startActivity(intent);
         });
 
         // 启动模式
-        Button btnLaunchMode = findViewById(R.id.btnLaunchMode);
-        btnLaunchMode.setOnClickListener(v -> {
+        binding.btnLaunchMode.setOnClickListener(v -> {
             Intent intent = new Intent(this, DemoLaunchModeUI.class);
             startActivity(intent);
         });
