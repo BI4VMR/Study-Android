e4893fdae6be39b11c3aaa6e469f3ae2aa834886
BI4VMR <bi4vmr@outlook.com>
2024-10-18 23:52:07 +0800

    EB986183-8780-426A-AD38-68368DF3AF69

--- M03_UI/C10_Compose/S01_Base/src/main/kotlin/net/bi4vmr/study/base/TestUIBase.kt
+++ M03_UI/C10_Compose/S01_Base/src/main/kotlin/net/bi4vmr/study/base/TestUIBase.kt
@@ -1,23 +1,15 @@
 package net.bi4vmr.study.base
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import net.bi4vmr.study.base.theme.TestComposeTheme
 
 /**
@@ -62,56 +54,6 @@ class TestUIBase : ComponentActivity() {
     fun GreetingPreview() {
         TestComposeTheme {
             Greeting("Android")
-        }
-    }
-
-    @Composable
-    @Preview(showBackground = true)
-    fun ClickableMessagePreview() {
-        TestComposeTheme {
-            ClickableMessage("如何解开安全带", true, {})
-        }
-    }
-
-    @Composable
-    fun ClickableMessage(
-        message: String,
-        isUserMe: Boolean,
-        authorClicked: (String) -> Unit
-    ) {
-        Surface(
-            color = MaterialTheme.colorScheme.primary,
-            shape = RoundedCornerShape(32.dp, 32.dp, 32.dp, 0.dp)
-        ) {
-            ClickableText(
-                text = AnnotatedString(message),
-                style = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
-                modifier = Modifier.padding(16.dp),
-                onClick = {
-                    Log.d("TestApp", "onClick")
-                }
-            )
-        }
-    }
-
-    @Composable
-    fun ClickableMessage(
-        message: String,
-        isUserMe: Boolean,
-        authorClicked: (String) -> Unit
-    ) {
-        Surface(
-            color = MaterialTheme.colorScheme.primary,
-            shape = RoundedCornerShape(32.dp, 32.dp, 32.dp, 0.dp)
-        ) {
-            ClickableText(
-                text = AnnotatedString(message),
-                style = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
-                modifier = Modifier.padding(16.dp),
-                onClick = {
-                    Log.d("TestApp", "onClick")
-                }
-            )
         }
     }
 }
