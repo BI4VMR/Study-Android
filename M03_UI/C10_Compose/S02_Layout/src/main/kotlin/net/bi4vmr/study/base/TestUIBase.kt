7e650aebf24bf72e7e33ff3d76f695527fc55407
BI4VMR <bi4vmr@outlook.com>
2024-10-20 23:42:07 +0800

    2266B210-B28D-4871-958C-211036EF1E59

--- M03_UI/C10_Compose/S02_Layout/src/main/kotlin/net/bi4vmr/study/base/TestUIBase.kt
+++ M03_UI/C10_Compose/S02_Layout/src/main/kotlin/net/bi4vmr/study/base/TestUIBase.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,11 +14,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.compose.ui.unit.toSize
 import net.bi4vmr.study.base.theme.TestComposeTheme
 
 /**
@@ -81,33 +86,23 @@ class TestUIBase : ComponentActivity() {
     fun RecommendSpeech(
         content: String
     ) {
-        ConstraintLayout {
-            // 创建引用，便于设置约束条件。
-            val (text, background) = createRefs()
+        BoxWithConstraints {
+            val textSize = remember { mutableStateOf(Size.Zero) }
 
             Box(
                 modifier = Modifier
                     .alpha(0.1F)
                     .background(MaterialTheme.colorScheme.error, RoundedCornerShape(24.dp))
-                    .constrainAs(background) {
-                        top.linkTo(text.top)
-                        bottom.linkTo(text.bottom)
-                        start.linkTo(text.start)
-                        end.linkTo(text.end)
-                    }
-            ){
-                Text(
-                    text = "“$content”",
-                    modifier = Modifier
-                        .padding(24.dp, 8.dp)
-                )
-            }
+                    .matchParentSize()
+            )
 
             Text(
                 text = "“$content”",
                 modifier = Modifier
                     .padding(24.dp, 8.dp)
-                    .constrainAs(text) {}
+                    .onGloballyPositioned { coordinates ->
+                        textSize.value = coordinates.size.toSize()
+                    }
             )
         }
     }
