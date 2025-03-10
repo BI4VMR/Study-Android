57812e8bfafb63463a5572bfb5aa6ceb3d631d91
BI4VMR <bi4vmr@outlook.com>
2025-03-10 18:09:00 +0800

    A236FD6B-42E7-4E80-B429-2EDC930644FF

--- M03_UI/C02_Resource/S05_Style/src/main/kotlin/net/bi4vmr/study/base/TestUIBaseKT.kt
+++ M03_UI/C02_Resource/S05_Style/src/main/kotlin/net/bi4vmr/study/base/TestUIBaseKT.kt
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import net.bi4vmr.study.R
 import net.bi4vmr.study.databinding.TestuiBaseBinding
 
+/**
+ * 测试界面：样式 - 基本应用。
+ *
+ * @author bi4vmr@outlook.com
+ * @since 1.0.0
+ */
 class TestUIBaseKT : AppCompatActivity() {
 
     companion object {
