c3f9b5833e232002960918b273907dbd9766bc3c
BI4VMR <bi4vmr@outlook.com>
2025-07-11 20:50:15 +0800

    48C3B0DC-2F34-4410-A376-3A708535C5DF

--- M02_Tool/C03_Test/S01_Base/build.gradle.kts
+++ M02_Tool/C03_Test/S01_Base/build.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
 val versionMinSDK: Int = agp.versions.minSdk.get().toInt()
 val versionCompileSDK: Int = agp.versions.compileSdk.get().toInt()
 val versionTargetSDK: Int = agp.versions.targetSdk.get().toInt()
@@ -10,7 +12,7 @@ plugins {
     alias(libAndroid.plugins.application)
     alias(libAndroid.plugins.kotlin)
     id("jacoco")
-    id("org.jetbrains.kotlinx.kover")
+    // id("org.jetbrains.kotlinx.kover")
 }
 
 android {
@@ -82,21 +84,30 @@ jacoco {
     toolVersion = "0.8.13"
 }
 
-// 在Gradle Test任务执行后自动执行Jacoco任务
+// 测试任务配置
 tasks.withType<Test> {
-    // jacoco.includeNoLocationClasses = true
-    // jacoco.excludes = listOf("jdk.internal.*")
+    // 允许测试失败后继续运行
     ignoreFailures = true
 
+    // 配置测试日志
     testLogging {
         events("passed", "skipped", "failed")
-        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        exceptionFormat = TestExceptionFormat.FULL
     }
 
+    // 在Test任务执行后自动执行Jacoco报告生成任务
     finalizedBy(tasks.named("jacocoTestReport"))
 }
 
-val fileFilter = listOf(
+
+val sourceCodeMainJava: String = "$projectDir/src/main/java/"
+val sourceCodeMainKotlin: String = "$projectDir/src/main/kotlin/"
+
+val buildDir: String = layout.buildDirectory.get().asFile.absolutePath
+val javaClassDir: String = "$buildDir/intermediates/javac/debug/classes/"
+val kotlinClassDir: String = "$buildDir/tmp/kotlin-classes/debug/"
+// 匹配需要排除的文件
+val excludeFilter: List<String> = listOf(
     "**/R.class",
     "**/R$*.class",
     "**/BuildConfig.*",
@@ -105,11 +116,17 @@ val fileFilter = listOf(
     "**/databinding",
     "**/*Activity.class",
 )
-
-val fileFilter2 = files(
-    "${layout.buildDirectory.get()}/intermediates/javac/debug/classes/net/bi4vmr/study/MainActivity.class"
+// 匹配在排除目录中但需要包含的文件
+val includeFilter: List<String> = listOf(
+    // "**/MainActivity.class",
 )
+// 组合生成需要统计的Java和Kotlin的字节码目录
+val javaClassFiles: FileTree =
+    fileTree(javaClassDir) { exclude(excludeFilter) } + fileTree(javaClassDir) { include(includeFilter) }
+val kotlinClassFiles: FileTree =
+    fileTree(kotlinClassDir) { exclude(excludeFilter) } + fileTree(kotlinClassDir) { include(includeFilter) }
 
+// 定义Jacoco报告生成任务
 tasks.register<JacocoReport>("jacocoTestReport") {
     group = "Reporting"
     description = "Generate Jacoco coverage reports after running tests."
@@ -118,33 +135,16 @@ tasks.register<JacocoReport>("jacocoTestReport") {
         xml.required = false
         csv.required = false
         html.required = true
-        html.outputLocation.set(file("${layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/html"))
+        html.outputLocation.set(file("$buildDir/reports/jacoco/jacocoTestReport/html/"))
     }
-
-    val debugTree = fileTree("${layout.buildDirectory.get()}/intermediates/javac/debug/classes") {
-        exclude(fileFilter)
-    } + fileFilter2
-
-    val kotlinDebugTree = fileTree("${layout.buildDirectory.get()}/tmp/kotlin-classes/debug") {
-        exclude(fileFilter)
-    }
-    debugTree.forEach {
-        println("Debug Tree: ${it.absolutePath}")
-    }
-    kotlinDebugTree.forEach {
-        println("KT Debug Tree: ${it.absolutePath}")
-    }
-
-    val sourceCodeMainJava = "$projectDir/src/main/java"
-    val sourceCodeMainKotlin = "$projectDir/src/main/kotlin"
 
     // 注册源码目录
     sourceDirectories.setFrom(files(sourceCodeMainJava, sourceCodeMainKotlin))
     // 注册字节码目录
-    classDirectories.setFrom(files(debugTree, kotlinDebugTree))
-    // 添加EC文件
+    classDirectories.setFrom(javaClassFiles + kotlinClassFiles)
+    // 注册EC文件
     executionData.setFrom(
-        fileTree(layout.buildDirectory.get()) {
+        fileTree(buildDir) {
             include(
                 "outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec",
                 "outputs/code_coverage/debugAndroidTest/connected/**/*.ec"
