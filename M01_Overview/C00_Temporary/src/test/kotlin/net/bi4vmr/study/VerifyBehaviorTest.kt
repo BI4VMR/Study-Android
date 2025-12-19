29a55570679aae6253a8513a23a04171862049be
BI4VMR <bi4vmr@outlook.com>
2025-12-19 17:52:43 +0800

    4664AE32-680B-449D-91D0-3FF929D5C9B0

---
+++ M01_Overview/C00_Temporary/src/test/kotlin/net/bi4vmr/study/VerifyBehaviorTest.kt
@@ -0,0 +1,17 @@
+package net.bi4vmr.study
+
+import org.junit.Test
+
+/**
+ * 验证行为。
+ *
+ * @author bi4vmr@outlook.com
+ * @since 1.0.0
+ */
+class VerifyBehaviorTest {
+
+    @Test
+    fun test() {
+
+    }
+}
