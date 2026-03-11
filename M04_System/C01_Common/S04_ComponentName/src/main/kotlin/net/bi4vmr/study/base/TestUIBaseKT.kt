d41d4824979a624cea4e8c969915dc7da8b8f9c0
BI4VMR <bi4vmr@outlook.com>
2026-03-11 15:54:31 +0800

    50271491-A08F-4FA3-93E6-8E1C83091D5C

---
+++ M04_System/C01_Common/S04_ComponentName/src/main/kotlin/net/bi4vmr/study/base/TestUIBaseKT.kt
@@ -0,0 +1,61 @@
+package net.bi4vmr.study.base
+
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import net.bi4vmr.study.databinding.TestuiBaseBinding
+
+/**
+ * 测试界面：TODO 添加简述。
+ *
+ * TODO 添加详情。
+ *
+ * @author bi4vmr@outlook.com
+ * @version 1.0
+ */
+class TestUIBaseKT : AppCompatActivity() {
+
+    companion object {
+        private val TAG: String = "TestApp-${TestUIBaseKT::class.java.simpleName}"
+    }
+
+    private val binding: TestuiBaseBinding by lazy {
+        TestuiBaseBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        with(binding) {
+            tvLog.movementMethod = ScrollingMovementMethod.getInstance()
+
+            btn01.setOnClickListener { test() }
+        }
+    }
+
+    private fun test() {
+        Log.i(TAG, "----- 功能模块 -----")
+        appendLog("\n----- 功能模块 -----")
+
+        // 示例代码...
+    }
+
+    // 向文本框中追加日志内容并滚动到最底端
+    private fun appendLog(text: Any) {
+        binding.tvLog.apply {
+            post { append("\n$text") }
+            post {
+                runCatching {
+                    val offset = layout.getLineTop(lineCount) - height
+                    if (offset > 0) {
+                        scrollTo(0, offset)
+                    }
+                }.onFailure { e ->
+                    Log.w(TAG, "TextView scroll failed!", e)
+                }
+            }
+        }
+    }
+}
