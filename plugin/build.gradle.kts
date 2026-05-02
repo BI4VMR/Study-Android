a223cf2c4d0811b0fc8ace3b7be82695f1188c2a
BI4VMR <bi4vmr@outlook.com>
2026-05-02 20:04:16 +0800

    A753B4BC-A8C7-496D-AB66-5799D6DE7046

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -3,9 +3,25 @@ plugins {
     `kotlin-dsl`
 }
 
+repositories {
+    // 腾讯云仓库镜像：Maven中心仓库+Spring+Google+JCenter
+    maven { setUrl("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+    // 阿里云仓库镜像：Gradle社区插件
+    maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin/") }
+    // 阿里云仓库镜像：Maven中心仓库+JCenter
+    maven { setUrl("https://maven.aliyun.com/repository/public/") }
+    // 阿里云仓库镜像：Google仓库
+    maven { setUrl("https://maven.aliyun.com/repository/google/") }
+
+    gradlePluginPortal()
+    mavenCentral()
+    google()
+}
+
 gradlePlugin {
     plugins {
-        /* Java版本配置 */
+
+        /* Java编译版本控制 */
         create("JavaVersion") {
             // 声明插件ID
             id = "net.bi4vmr.gradle.plugin.java.version"
@@ -33,15 +49,4 @@ gradlePlugin {
             implementationClass = "net.bi4vmr.gradle.plugin.PrivatePublishPlugin"
         }
     }
-
-    sourceSets {
-        getByName("main") {
-            kotlin.srcDir("src/main/kotlin")
-        }
-    }
-}
-
-dependencies {
-    // 此处的依赖仅用于确保自定义插件能够访问相关API，不会影响插件调用者所使用的实际版本。
-    compileOnly("com.android.tools.build:gradle-api:8.13.1")
 }
